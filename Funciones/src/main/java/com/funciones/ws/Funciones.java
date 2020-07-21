8de2598d3368d52bdf423b504acf8c230aaf9639
alexandergil182 <agilta@ut.edu.co>
2020-07-21 14:31:08 -0500

    Prueba

--- Funciones/src/main/java/com/funciones/ws/Funciones.java
+++ Funciones/src/main/java/com/funciones/ws/Funciones.java
@@ -9,7 +9,15 @@ import javax.jws.WebService;
 public interface Funciones {
 	
 	@WebMethod
-	public double operacion(int y2, int y1, int x2,int x1);
+	public double pendiente(int y2, int y1, int x2,int x1);
 	
-
+	@WebMethod
+	public double promedio(int valor1, int valor2, int valor3,int valor4);
+	
+	@WebMethod
+	public float triangulo(float base, float altura, float area);
+	
+	
+	
+	
 }
--- Funciones/src/main/java/com/funciones/ws/FuncionesImpl.java
+++ Funciones/src/main/java/com/funciones/ws/FuncionesImpl.java
@@ -1,5 +1,57 @@
 package com.funciones.ws;
 
-public class FuncionesImpl {
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.funciones.ws.Funciones")
+public class FuncionesImpl implements Funciones {
+
+	
+
+	private double r1;
+
+	public double pendiente(int y2, int y1, int x2, int x1) {
+		
+		double resultado = 0;
+		
+		double	r1 = y2-y1;
+		double	r2 = x2-x1;
+		double	r3 = r1/r2;
+			
+			resultado = r3;
+			
+			return resultado;
+		
+		
+	}
+
+	public double promedio(int valor1, int valor2, int valor3, int valor4) {
+		
+		double resultado = 0;
+		double promedio = 4;
+		
+		
+	r1 = (valor1 + valor2 + valor3 + valor4) / promedio; 
+		
+		r1 = resultado;
+		
+		return resultado;
+	}
+
+	public float triangulo(float base, float altura, float area) {
+		
+	 float  resultado = 0;
+		
+		base  = 7;
+		altura = 10;
+		area = (7 * 10)/2;
+		
+		area =  resultado;
+		
+		return resultado;
+	}
+	
+	
+	
+	
 
 }
